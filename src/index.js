c4120b0958ec6a2b4a350b469654c923f6444775
Pedro Rossi <pgrr@cin.ufpe.br>
2018-07-04 22:38:17 -0300

    Search page almost done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Search from './containers/Search';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Search />, document.getElementById('root'));
+if (process.env.NODE_ENV === 'production')
+    registerServiceWorker();
